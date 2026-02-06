c16e0b89050bb588004958b62f3705c86c67ba44
ankitojha116 <ojhaankit.116@gmail.com>
2026-02-05 19:38:42 -0500

    Changed USerPOJO name to UserBean for better clarity.

--- src/main/java/com/demo/csv/UserBean.java
+++ src/main/java/com/demo/csv/UserBean.java
@@ -1,17 +1,11 @@
 package com.demo.csv;
 
-public class UserPOJO {
+public class UserBean {
 	private String username;
 	private String password;
 	
-	public UserPOJO() {
+	public UserBean() {
 		
-	}
-
-	public UserPOJO(String username, String password) {
-		super();
-		this.username = username;
-		this.password = password;
 	}
 
 	public String getUsername() {
