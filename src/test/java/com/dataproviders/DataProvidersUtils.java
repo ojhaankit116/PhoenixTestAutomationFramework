21b30bd74c91e8a23cc9cd0d1517a313e3f1d26c
ankitojha116 <ojhaankit.116@gmail.com>
2026-02-22 06:56:35 -0500

    Updated the generateFakeProblemList()
    Now it can create Problem List with different size.
    Each create job payload will have different problem array.

--- src/test/java/com/dataproviders/DataProvidersUtils.java
+++ src/test/java/com/dataproviders/DataProvidersUtils.java
@@ -47,7 +47,7 @@ public class DataProvidersUtils {
 	
 	@DataProvider(name = "CreateJobAPIFakerDataProvider", parallel = true)
 	public static Iterator<CreateJobPayload> createJobFakeDataProvider() {
-		Iterator<CreateJobPayload> payloadIterator = FakerDataGenerator.generateFakeCreateJobData(100);
+		Iterator<CreateJobPayload> payloadIterator = FakerDataGenerator.generateFakeCreateJobData(5);
 		return payloadIterator;
 	}
 }
